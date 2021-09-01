1cdacd503c204e7d286c22f5eae069168827378f
Tipn <tito.unr@gmail.com>
2021-09-01 17:15:45 +0300

    6.7.8.9

---
+++ lesson_7.py
@@ -0,0 +1,21 @@
+# test = None
+# test = 123
+# print(test)
+
+# X=1; y=5  
+# x, y =(1,5)
+
+#bool boolian
+my_true = True
+my_false = False
+ 
+# print (type (my_true))
+# print (type (my_false))
+
+# str() int() float() bool() 
+
+x= 5.2
+print (x, type(x))
+
+x=int(x)
+print (x, type(x))
---
+++ lesson_9.py
@@ -0,0 +1,37 @@
+# операции со строками 
+
+# s = r'C:\d\new.txt'
+
+# s = 'py' 'thon'
+# print(s)
+
+# s1 = 'Hello,'
+# s2 = 'world!'
+# s3 = s1+s2 
+# print (s3)
+
+# name ='John'
+# age = 33
+# print('My nameis ' + name + "I'm " +str(age))
+
+# print ("hi "*5)
+
+# s='Hello world!'
+# print(s[11])
+# print(s[-12])
+# s[0] ='D'
+
+# [X:Y:Z]
+
+s='Hello world!'
+print (s[0:11]) #Hello world
+print (s[0:-1]) #!
+print (s[0:5]) #Hello 
+print (s[6:]) #world!
+print (s[::2]) #Hlowrd
+print (s[::-1]) #!dlrow olleH
+print (s[:5] + s[6:]) #!dlrow olleH
+# print (s[::-1]) #!dlrow olleH
+
+
+
