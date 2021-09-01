7ecbe22f8b566ab25bcccad39db82f81446a4b01
Tipn <tito.unr@gmail.com>
2021-09-01 17:16:35 +0300

    10

---
+++ lesson_10.py
@@ -0,0 +1,12 @@
+# методы строк
+
+# len(S) 
+s='hello'
+# print (len(s)) #5
+# s2=s.capitalize()
+# print (s, s2)   #hello Hello
+
+# print(s.center(20, '!')) #!!!!!!!hello!!!!!!!!
+
+# print (s.count('l')) #2
+print(s.find('1'))
