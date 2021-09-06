4563537ff951acafcc1b7194abfd0ed1ee386e0c
Tipn <tito.unr@gmail.com>
2021-09-06 18:14:31 +0300

    12 True or False

--- lesson_12.py
+++ lesson_12.py
@@ -2,5 +2,18 @@
 # True
 # False
 
+# == #
 
-print ('Titel')
+print (2 > 3) # False
+print (2 < 3) # True
+print (3 == 3) # True
+
+print (3 == 3) # True
+'''
+if выражение1:
+    блок кода1
+elif выражение 2:
+    блок кода 2
+else:
+    блок кода 3
+'''
