51b67674c17fb695eff201c0ee13e2766276b855
Tipn <tito.unr@gmail.com>
2021-09-06 18:50:17 +0300

    12 операторы или и

--- lesson_12.py
+++ lesson_12.py
@@ -34,12 +34,25 @@ else:
 # elif light ==('green'):
 #     print ('Go!')
 # else:
-#     print ('What?')
+# #     print ('What?')
 
-age = int(input('сколько вам лет?'))
-x = 18 - age
-if age >=18:
-    prnt('Добро пожаловать')
-else:
-    print (f'Вам {age}, вам осталось {18-age} лет')
+# age = int(input('сколько вам лет?'))
+# x = 18 - age
+# if age >=18:
+#     prnt('Добро пожаловать')
+# else:
+#     print (f'Вам {age}, вам осталось {18-age} лет')
     
+time = 11
+if time < 12 or time > 13 :  # любое условие должно вернуть тру и будет тру ИЛИ
+    print ('Open')
+else:
+    print('Close') 
+
+time = 12
+day = 'st'
+
+if time >= 8 and day != 'su' :  # каждое условие должно вернуть тру и будет тру И
+    print ('Open')
+else:
+    print('Close') 
