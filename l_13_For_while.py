fa8eb1031732d669cf8cbe5c598f8b5e87963982
Tipn <tito.unr@gmail.com>
2021-09-07 17:07:55 +0300

    13 for + spece

--- l_13_For_while.py
+++ l_13_For_while.py
@@ -2,11 +2,19 @@
 
 # while stmt: 
     # do...
+# https://pythonz.net/references/named/print
 
-i=1
-while i <= 10:
-    print (i) 
+# i=1
+# while i <= 10:
+    # print (i, end=' ') 
     # i = i+1
-    i += 1
+    # i += 1
+
+# print ('hello', 'world', sep='!', end=' | ')
+# print ('hello2', 'world2')
+
+s= 'Hellow world'
+for l in s:   # для каждой буквы в строке s
+    print (l, end=' ')  # H e l l o w   w o r l d 
 
     
