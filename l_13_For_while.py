26f0147002ea9b5bb2e7038edc2cc723147a4752
Tipn <tito.unr@gmail.com>
2021-09-07 17:26:36 +0300

    13 for + else

--- l_13_For_while.py
+++ l_13_For_while.py
@@ -13,10 +13,15 @@
 # print ('hello', 'world', sep='!', end=' | ')
 # print ('hello2', 'world2')
 
-s= 'Hellow world'
-for l in s:   # для каждой буквы в строке s
-    if l == ' ':
-        continue
-    print (f'"{l}"', end=' ')  # H e l l o w   w o r l d 
-
+# s= 'Hellow world'
+# for l in s:   # для каждой буквы в строке s
+    # if l == ' ':
+        # continue
+    # print (f'"{l}"', end=' ')  # "H" "e" "l" "l" "o" "w" "w" "o" "r" "l" "d"
     
+for i in 'Hello world' :
+    if i == ' ':
+        break 
+    print (i, end=' ') # работает в случае есле в переменной нет пробелов
+else :
+    print ('\nNo spaces') # работает в случае есле в переменной нет пробелов
