7767008c19d82c5b469d92d61bdb05018ba89b77
Tipn <tito.unr@gmail.com>
2021-09-07 17:52:33 +0300

    14 list generator with conditions

--- l_14_lists.py
+++ l_14_lists.py
@@ -6,7 +6,10 @@ l = [1, 2, 3, 'hello', ['test', 10], 'word', True]
 l2 = list ('hello')
 l3 = list ((1,2,3))
 l4 = [i for i in 'hello'] # использовали генератор списков
-l5 = [i for i in 'hello world'] # 
+l5 = [i for i in 'hello world' if i != ' '] # без пробелов
+l6 = [i for i in 'hello world' if i != ' ' and i !='e'] # без пробелов и без е
+l7 = [i for i in 'hello world' if i not in ['a', 'e', 'i', 'u', 'o', ' ']] # без пробелов и без глассных
 
 
-print (l, l2, l3, l4, l5, sep='\n' )
+
+print (l, l2, l3, l4, l7, sep='\n' )
