f1a444971431cc8c085742d63393d8c640203e1d
Tipn <tito.unr@gmail.com>
2021-09-07 18:55:51 +0300

    14 HW not complite

--- l_14_lists.py
+++ l_14_lists.py
@@ -1,15 +1,38 @@
 #переменные. списки это масивы 
 # список можно сравнить с картотекой, где у каждой карточки есть свое значение илил с много квартирным домом
  
-l = [1, 2, 3, 'hello', ['test', 10], 'word', True] 
+# l = [1, 2, 3, 'hello', ['test', 10], 'word', True] 
 
-l2 = list ('hello')
-l3 = list ((1,2,3))
-l4 = [i for i in 'hello'] # использовали генератор списков
-l5 = [i for i in 'hello world' if i != ' '] # без пробелов
-l6 = [i for i in 'hello world' if i != ' ' and i !='e'] # без пробелов и без е
-l7 = [i for i in 'hello world' if i not in ['a', 'e', 'i', 'u', 'o', ' ']] # без пробелов и без глассных
+# l2 = list ('hello')
+# l3 = list ((1,2,3))
+# l4 = [i for i in 'hello'] # использовали генератор списков
+# l5 = [i for i in 'hello world' if i != ' '] # без пробелов
+# l6 = [i for i in 'hello world' if i != ' ' and i !='e'] # без пробелов и без е
+# l7 = [i for i in 'hello world' if i not in ['a', 'e', 'i', 'u', 'o', ' ']] # без пробелов и без глассных
 
-print (l, l2, l3, l4, l7, sep='\n' )
+# print (l, l2, l3, l4, l7, sep='\n' )
 
-print (list(range(1,11,2)))
+# print (list(range(1,11,2)))
+
+# for i in range(1,3):
+#     print(f'внешний цикл#{i}')
+#     for j in range(1,3):
+#         print(f'\tвнутренний цикл #{j}')
+#         '''output:
+# внешний цикл#1
+#         внутренний цикл #1
+#         внутренний цикл #2
+# внешний цикл#2
+#         внутренний цикл #1
+#         внутренний цикл #2
+#         '''
+# ДЗ написать код который будет выводить таблицу умножения 
+x=1
+y=2
+while x <=11 and y <=11:
+    print ('{x}' '*' '{y}' '=' (x*y) 
+    x =  x+1
+# y=1
+# for x in range(x+1):
+    # print (f x*y)
+    # for y in range(y+1)
