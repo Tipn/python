532df78af748866ee438ad3d755d13d4f19ca0ce
Pavel <tito.unr@gmail.com>
2021-09-14 16:48:13 +0300

    генератор для создания словарей

--- l_23_dictionary.py
+++ l_23_dictionary.py
@@ -5,6 +5,8 @@
 d = {} # <class 'dict'>
 product1 = {'title': 'Sony', 'price': 1000} #способы создания словарей 
 product2 = dict(title='iphone', price=110) # кавычки не нужны 
+product3 = dict.fromkeys (['price1','price2', 'price3'], 50) # создать словарь с одинаковым значением для всех елементов 
+print(product3) #{'price1': 50, 'price2': 50, 'price3': 50}
 
 users = [ #{'ata@gmail.com': 'ata', 'kety@gmail.com': 'kety', 'john@gmail.com': 'john'}
     ['ata@gmail.com', 'ata'],  # красивая запись длясоздания словаря
@@ -16,3 +18,6 @@ print(d_users)
 print(d)
 print(product1)
 print(product2)
+
+nums = {i: i+1 for i in range (1,10)} #  генератор для создания словарей 
+print (nums) #{1: 2, 2: 3, 3: 4, 4: 5, 5: 6, 6: 7, 7: 8, 8: 9, 9: 10}
