decf54ae2defd1baf8b0c4486aa6e737ee90a74e
Pavel <tito.unr@gmail.com>
2021-09-23 22:00:04 +0300

    T F bool

--- l_7_True_False.py
+++ l_7_True_False.py
@@ -1,21 +1,24 @@
-# test = None
-# test = 123
+# test = None   # отсутствие значения 
+# test = 123    # а после указать значение 
 # print(test)
 
-# X=1; y=5  
-# x, y =(1,5)
+# X=1; y=5      # способы указания нескольких перерменных 
+# x, y =(1,5)   # распаковка кортежа
 
-#bool boolian
-my_true = True
-my_false = False
+#bool boolean тип данных который принимает истину или лож
+my_true = True  # в скобки брать не нужно потому как это и есть значение
+my_false = False#     
  
 # print (type (my_true))
 # print (type (my_false))
 
-# str() int() float() bool() 
+# str() int() float() bool() # функцие приводят к указанному типу 
 
 x= 5.2
 print (x, type(x))
 
-x=int(x)
+x=int(x)            # изменение типа данных приведение к целым числам 
+print (x, type(x))
+
+x=bool(x)            # изменение типа данных приведение к значению T or F
 print (x, type(x))
