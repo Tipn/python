752e16e422c9038f9e74a84fbfc55c0e4c6f6819
Pavel <tito.unr@gmail.com>
2021-09-26 00:55:18 +0300

    27

--- l_27_user_functions_2.py
+++ l_27_user_functions_2.py
@@ -1,12 +1,19 @@
-def set_register(s):
-    if ' ' in s:
-        return s.upper()
-    else:
-        return s.lower()
-print(set_register('hello world'))
-print(set_register('hello,world'))
+# def set_register(s):
+#     if ' ' in s:
+#         return s.upper()
+#     else:
+#         return s.lower()
+# print(set_register('hello world'))
+# print(set_register('hello,world'))
 
-def get_sum(a, b, c, d):
+def get_sum(a, b, c, d):        # позицыонные аргументы каждая буква соответсвует цыфре ниже
+    return a + b + c + d
+# print(get_sum(1, 2, 5, 7)) # если не передать 1 элемент то будет ошибка 
+
+def get_sum(a, b, c=0, d=1): # есть именнованые аргументы
     return a + b + c + d
 
-print(get_sum(1, 2, 5, 7))
+print(get_sum(5, 2)) # поскольку именнованые уже заданы сумма будет по всем остальным 
+
+
+print('hello', end='', sep='') # именнованые аргументы должны следовать после позицыонных 
