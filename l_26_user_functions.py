f186473f28d7f889ebc0dd922a3a8ccc389b1cbb
Pavel <tito.unr@gmail.com>
2021-09-25 14:54:14 +0300

    functions

---
+++ l_26_user_functions.py
@@ -0,0 +1,17 @@
+# s = 'Hello world'
+# if ' ' in s:
+#     s = s.upper()
+# else:
+#     s = s.lower()
+# print(s)
+
+# s2 = 'Hello world'        # это не правильно !
+# if ' ' in s2:             # нужно пользоваться функцыями
+#     s2 = s2.upper()       
+# else:
+#     s2 = s2.lower()
+# print(s2)
+def hello():   # defing oпределять 
+    print ('Hello')
+
+hello()
