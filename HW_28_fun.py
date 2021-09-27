1531ef5a0e381973541ea66001e2f64f2f57066c
Tipn <tito.unr@gmail.com>
2021-09-27 16:10:09 +0300

    HW new

---
+++ HW_28_fun.py
@@ -0,0 +1,12 @@
+# дан масив, в котором среди прочих элементов есть слово "odd" (нечетный). Определите, есть ли в списке число, 
+# которое является индексом элемента "odd". Напишите функцыю, которая будет возвращать True or False, соответсвенно.
+
+def odd_ball(arr):
+    # return arr.index('odd')
+    retern 10 in arr
+
+# print(["even",4,"even",7,"even",55,"even",6,"even",10,"odd",3,"even"].index('odd'))
+
+print(["even",4,"even",7,"even",55,"even",6,"even",9,"odd",3,"even"].index('odd'))
+
+print(10 in ["even",4,"even",7,"even",55,"even",6,"even",9,"odd",3,"even"].index('odd'))
