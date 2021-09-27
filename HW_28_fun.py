afbe26e736488042b0fd4ab0257a3af17b6bce50
Tipn <tito.unr@gmail.com>
2021-09-27 16:30:39 +0300

    HW функция найти

--- HW_28_fun.py
+++ HW_28_fun.py
@@ -2,11 +2,12 @@
 # которое является индексом элемента "odd". Напишите функцыю, которая будет возвращать True or False, соответсвенно.
 
 def odd_ball(arr):
-    # return arr.index('odd')
-    retern 10 in arr
+    return arr.index('odd') in arr #первая задача с масивом функции 
+    # return 10 in arr # 
 
-# print(["even",4,"even",7,"even",55,"even",6,"even",10,"odd",3,"even"].index('odd'))
+print(odd_ball(["even",4,"even",7,"even",55,"even",6,"even",10,"odd",3,"even"])) # True
+print(odd_ball(["even",4,"even",7,"even",55,"even",6,"even",9,"odd",3,"even"]))  # False
+print(odd_ball(["even",10,"odd",2,"even"]))          # True
 
-print(["even",4,"even",7,"even",55,"even",6,"even",9,"odd",3,"even"].index('odd'))
 
-print(10 in ["even",4,"even",7,"even",55,"even",6,"even",9,"odd",3,"even"].index('odd'))
+# print(10 in ["even",4,"even",7,"even",55,"even",6,"even",9,"odd",3,"even"])
