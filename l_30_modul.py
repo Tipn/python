97e9a5eafb645f4ffe68be933a87892ce301707c
Pavel <tito.unr@gmail.com>
2021-09-29 16:34:20 +0300

    перемешать все

--- l_30_modul.py
+++ l_30_modul.py
@@ -6,8 +6,15 @@
 
 # переписать игру угадай число, с модулем rendom. 
 
-import os, random
+import os #, random # лучше каждый модуль отдельно записывать
+# import random as r # возможно использовать псевдонимы
+from random import randint, shuffle
+
 
 print(os.getcwd())
-print(random.randint(1, 100)) # генерируем случайное число 
+# print(r.randint(1, 100)) # генерируем случайное число 
 
+print(randint(1, 100))
+l = [1, 2, 3, 4, 5]
+shuffle(l)      # перемешать все значения 
+print(l)
