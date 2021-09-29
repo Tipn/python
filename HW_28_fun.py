6dc0632d1b37470d9856b2dfb1daae0d6a403ba1
Pavel <tito.unr@gmail.com>
2021-09-29 09:29:47 +0300

    другое решение_

--- HW_28_fun.py
+++ HW_28_fun.py
@@ -26,8 +26,13 @@ def find_sum(n):
             res += i
     return res
 
+def find_sum2(n):
+    return sum(i for i in range(n + 1) if i % 3 == 0 or i % 5 == 0)
+
 print(find_sum(5)) # return 8 (3 + 5)
 print(find_sum(10)) # return 33 (3 + 5 + 6 + 9 + 10)
+print(find_sum2(5)) # return 8 (3 + 5)
+print(find_sum2(10)) # return 33 (3 + 5 + 6 + 9 + 10)
 
 """
 3 Дан список имен. Выберете в новый список только те имена, которые состоят из 4-х букв.
