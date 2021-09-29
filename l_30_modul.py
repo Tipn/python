71732012607bedb6c2cbb96c807a4f936f9d3a48
Pavel <tito.unr@gmail.com>
2021-09-29 17:30:23 +0300

    вызов действий

--- l_30_modul.py
+++ l_30_modul.py
@@ -5,16 +5,29 @@
 # module OS 
 
 # переписать игру угадай число, с модулем rendom. 
-
+""" 
 import os #, random # лучше каждый модуль отдельно записывать
 # import random as r # возможно использовать псевдонимы
 from random import randint, shuffle
 
 
-print(os.getcwd())
+print(os.getcwd()) # вывести путь где находится 
 # print(r.randint(1, 100)) # генерируем случайное число 
 
 print(randint(1, 100))
 l = [1, 2, 3, 4, 5]
 shuffle(l)      # перемешать все значения 
 print(l)
+"""
+
+# import libs_30 
+# print(libs_30.get_count('hello', '1'))
+# print(libs_30.get_len('hello'))
+
+from libs_30 import get_count, get_len  # второй вариант вызова модуля 
+print(get_count('hello', '1'))
+print(get_len('hello'))
+"""
+при выполнении модуля его код из libs копируется в файл где был вызван libs 
+
+"""
