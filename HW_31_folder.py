6e1f2617fb7603982caa9ecdcad7ae982fee8947
Pavel <tito.unr@gmail.com>
2021-09-30 17:55:58 +0300

    init HW

---
+++ HW_31_folder.py
@@ -0,0 +1,15 @@
+from typing import Text
+
+
+module OS
+metod walk 
+
+надо получить дерево каталога 
+
+folder 
+    file1.txt
+    subfolder
+        2.Text
+        3.Text
+    subfolder
+metod walk 
