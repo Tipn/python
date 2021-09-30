c03d9e7f9a8bde890dc9629fa175a4dadf53f592
Pavel <tito.unr@gmail.com>
2021-09-30 16:37:31 +0300

    date_time

---
+++ l_31_core_modul.py
@@ -0,0 +1,16 @@
+# datetime
+from datetime import date, datetime
+"""
+# date
+today = date.today()
+print(today) #2021-09-30
+print(today.day) #30
+print(today.month) #9
+print(today.year) #2021
+print(today.weekday()) #3
+"""
+datetime
+now = datetime.now()
+now2 = datetime.today()
+
+print(now, now2, sep='\n')
