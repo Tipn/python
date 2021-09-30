97947012a16e5f8d074888daccd2f714150be142
Pavel <tito.unr@gmail.com>
2021-09-30 16:46:23 +0300

    date_sec

--- l_31_core_modul.py
+++ l_31_core_modul.py
@@ -14,3 +14,10 @@ now = datetime.now()
 now2 = datetime.today()
 
 print(now, now2, sep='\n')
+print(now) #2021-09-30 16:37:46.612994
+print(now.day) #30
+print(now.month) #9
+print(now.year) #2021
+print(now.hour) #16
+print(now.minute) #45
+print(now.second) #50
