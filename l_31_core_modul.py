319ac96b88c817dba7eafe8c4f4723341ab521b9
Pavel <tito.unr@gmail.com>
2021-09-30 16:52:12 +0300

    strftime

--- l_31_core_modul.py
+++ l_31_core_modul.py
@@ -12,7 +12,7 @@ print(today.weekday()) #3
 datetime
 now = datetime.now()
 now2 = datetime.today()
-
+"""
 print(now, now2, sep='\n')
 print(now) #2021-09-30 16:37:46.612994
 print(now.day) #30
@@ -21,3 +21,9 @@ print(now.year) #2021
 print(now.hour) #16
 print(now.minute) #45
 print(now.second) #50
+
+days = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс']
+print(days[now.weekday()]) # чт
+"""
+
+strftime
