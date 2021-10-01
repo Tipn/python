f1ea53c013cea78b6004caf88e0a50033b49dfc1
Pavel <tito.unr@gmail.com>
2021-10-01 14:00:49 +0300

    folder tree

--- HW_31_folder.py
+++ HW_31_folder.py
@@ -13,4 +13,17 @@ folder
         2.Text
         3.Text
     subfolder
-metod walk """
+metod walk 
+on 
+!!! https://pythoner.name/walk  метод возвращает обэкт генератор, 
+из которого получают кортеджи для каждого каталога переданой файловой иерархии.
+"""
+import os
+tree = os.walk('folder')
+for files in tree:
+    print(files)    #('folder', ['subfolder2', 'subfolder1'], [])
+                    # ('folder/subfolder2', ['subfolder2-1'], ['1.txt'])
+                    # ('folder/subfolder2/subfolder2-1', [], ['4.txt'])
+                    # ('folder/subfolder1', [], ['3.txt', '2.txt'])
+
+print(tree)
