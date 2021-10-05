78232720b407b9f414bb355a936804bf967a50e8
Pavel <tito.unr@gmail.com>
2021-10-05 21:51:35 +0300

    чтение символа в файле

--- l_33_work_with_File.py
+++ l_33_work_with_File.py
@@ -1,3 +1,10 @@
 # работа с файлами 
-# https://pythonze.net/references/named/open/ 
+# https://pythonz.net/references/named/open/ 
 # после открытия файл его нужно закрывать!! 
+
+f = open('file.txt')
+text = f.read(1)
+print(f.encoding)
+f.close
+
+print(text)
