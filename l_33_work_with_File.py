88b33bf40a8e54e83847966d508627d9e06bc74a
Pavel <tito.unr@gmail.com>
2021-10-05 22:00:21 +0300

    чтения файла целиком

--- l_33_work_with_File.py
+++ l_33_work_with_File.py
@@ -1,10 +1,20 @@
 # работа с файлами 
 # https://pythonz.net/references/named/open/ 
 # после открытия файл его нужно закрывать!! 
+'''
+f = open('file.txt', encoding='utf-8')
+text = f.read(2)
+text2 = f.read(8)
+print(f.encoding) # UTF-f
 
-f = open('file.txt')
-text = f.read(1)
-print(f.encoding)
 f.close
 
-print(text)
+print(text) # Ст
+print(text2) # рока 1
+'''
+f = open('file.txt', 'r', encoding='utf-8')
+text = f.read() # прочитать весь файл целиком 
+
+f.close
+
+print(text) # Ст
