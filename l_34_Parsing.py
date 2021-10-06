ee5af8053b9991144ee373138dc9e0e6531d87cc
Pavel <tito.unr@gmail.com>
2021-10-06 12:36:33 +0300

    добавил ссылок

--- l_34_Parsing.py
+++ l_34_Parsing.py
@@ -1,2 +1,4 @@
 # парсинг 
-# https://ru.wikipedia.org/wiki/pip_(менеджер_пакетов)
+# https://ru.wikipedia.org/wiki/pip_(менеджер_пакетов) - менеджер пакетов система управления пакетов
+# https://pypi.org
+# beautifulsoup4.pypl
