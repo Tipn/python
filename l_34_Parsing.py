c9014d6ebc2e29784351c8dc0336ac3142a2439a
Tipn <tito.unr@gmail.com>
2021-10-08 16:52:55 +0300

    reqest from bs4

--- l_34_Parsing.py
+++ l_34_Parsing.py
@@ -1,5 +1,11 @@
-# парсинг 
+# парсинг данных с ресурса -  
 # https://ru.wikipedia.org/wiki/pip_(менеджер_пакетов) - менеджер пакетов система управления пакетов
-# https://pypi.org
+# https://pypi.org аналог composer 
 # beautifulsoup4.pypl
+# https://docs.python.org/3/library/urllib.request.html - познакомится с модулем 
 # надо установиь питон по умолчанию после продолжу 
+from base64 import BeautifulSoup
+import urllib.request 
+
+req = urllib.request.urlopen('https://www.ua-football.com/sport')
+print(req)
