a92e701b9623aad50865189df8ae79ba5d352c6f
Tipn <tito.unr@gmail.com>
2021-10-08 17:32:40 +0300

    only news

--- l_34_Parsing.py
+++ l_34_Parsing.py
@@ -16,5 +16,6 @@ req = urllib.request.urlopen('https://www.ua-football.com/sport')
 
 html = req.read()
 
-soup =BeautifulSoup(html)
-print(soup) 
+soup = BeautifulSoup(html, 'html.parser')
+news = soup.find_all('li', class_='liga-news-item')
+print(news)
