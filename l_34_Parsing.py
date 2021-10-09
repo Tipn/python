48a35e49b82b155085b33ee34f8cf10d5133b8a0
Pavel <tito.unr@gmail.com>
2021-10-09 09:19:20 +0300

    описание че там на делал

--- l_34_Parsing.py
+++ l_34_Parsing.py
@@ -7,23 +7,22 @@
 # https://docs.python.org/3/library/urllib.request.html - познакомится с модулем 
 # надо установиь питон по умолчанию после продолжу 
 # pythonz.net/references/named/string.strip 
+from os import close
 from bs4 import BeautifulSoup
 import urllib.request 
 
 req = urllib.request.urlopen('https://www.ua-football.com/sport')
-
 # print(req) # проверка все ли работает выдаст запрос 
 # print(html) # в одну строку выдаст всю страничку без переносов 
-
-html = req.read()
+html = req.read()  # функция получает запрос 
 
 soup = BeautifulSoup(html, 'html.parser')
-news = soup.find_all('li', class_='liga-news-item')
+news = soup.find_all('li', class_='liga-news-item') # ф. которая парсит данные 
 # print(news)
 
 results = []
 
-for item in news:
+for item in news: # ф. которая записывает данные в файл 
     title = item.find('span', class_='d-block').get_text(strip=True)
     desc = item.find('span', class_='name-dop').get_text(strip=True)
     href = item.a.get('href')
@@ -38,4 +37,5 @@ for item in results:
     f.write(f'Новость № {i}\n\nНазвание: {item["titel"]}\nОписание: {item["desc"]}\n\Силка:
     {item["href"]}')
         i += 1
+f.close()
 # print(results) # распечаттаь масив словарей 
