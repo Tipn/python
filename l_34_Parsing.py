041e9428f258d1f586b374cbe5b086c199bce9da
Pavel <tito.unr@gmail.com>
2021-10-09 09:08:04 +0300

    причесать

--- l_34_Parsing.py
+++ l_34_Parsing.py
@@ -24,6 +24,11 @@ news = soup.find_all('li', class_='liga-news-item')
 results = []
 
 for item in news:
-    title = item.find('span', class_='d-block').get_text()
-    desc = None
-    href = None
+    title = item.find('span', class_='d-block').get_text(strip=True)
+    desc = item.find('span', class_='name-dop').get_text(strip=True)
+    href = item.a.get('href')
+    results.append({
+            'title': title,
+            'desc': desc,
+            'href': href,
+    })
