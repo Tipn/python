d788f9f80cae97ba6d600c94eb70715783552b68
Pavel <tito.unr@gmail.com>
2021-10-10 18:55:38 +0300

    как работать с исключениями

--- l_35_mistaks_exemptions.py
+++ l_35_mistaks_exemptions.py
@@ -8,10 +8,21 @@
 # print(1 + '2') # исключения TypeError: 
 
 # print(int('test')) # исключения ValueError:
-
+'''
 try:                        # способ попробывать какойто кусок кода который может привести к ошибки
     num = 100 / 0           # ошибочный код
 except ZeroDivisionError:   # описание при какой ошибки должно действовать исключение 
+# except Exception:         # все исключения !
     num = 0                 # решение ошибочного значения 
 print(num)                  # вывод без ошибки 
 print('hi')
+'''
+
+try:
+    num = 100 / '2'
+except Exception:
+    num = 0
+else:                   # else отрабатывает если код без ошибок 
+    print('Else')
+finally:
+    print('Finally')
