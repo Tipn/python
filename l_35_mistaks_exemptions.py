11bd46a787f612381aa0d36a7066d31289b47442
Pavel <tito.unr@gmail.com>
2021-10-10 18:41:17 +0300

    2 исключения

--- l_35_mistaks_exemptions.py
+++ l_35_mistaks_exemptions.py
@@ -3,4 +3,8 @@
 
 # print(100 / 10 # пример синтаксической или критической ошибки 
 
-print(100 / 0 )#
+# print(100 / 0 )# исключения ZeroDivisionError: division by zero
+
+# print(1 + '2') # исключения TypeError: 
+
+print(int('test'))
