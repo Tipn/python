dcfefc3e1a75ab6cf1fd74513556b231b433855f
Tipn <tito.unr@gmail.com>
2021-10-11 17:45:53 +0300

    вызов свойства

--- l_36_OOP_class_object.py
+++ l_36_OOP_class_object.py
@@ -7,10 +7,14 @@
 class A:
     property1 = 'Propery 1'
     property2 = 'Propery 2'
+    name = 'geest'
 
+    def say_hi(self, name=''):   # self является обязательнымчтоб редактор понимал на какой обект ссылться 
+        if name:
+            return 'Hi, ' + name
+        else:
+            return 'Hello, ' + self.name
 
-    def say_hi(self, name='guest'):   # self является обязательнымчтоб редактор понимал на какой обект ссылться 
-        return 'Hi, ' + name
 
 
 a = A() # был создан объект<__main__.A object at 0x7f22c67d6040>
@@ -19,5 +23,5 @@ b = A()
 # a.property2 = 'Propery 2'
 # print(a)
 print(a.property1)  # создал свойство
-print(a.say_hi('Pasha')) # name обязательный параметр
-print(b.say_hi('John'))
+print(a.say_hi()) # name обязательный параметр
+print(b.say_hi('Pasha'))
