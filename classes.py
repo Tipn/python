8f249cee5d100ca606b85abee6a4c181c66f119c
Tipn <tito.unr@gmail.com>
2021-10-12 13:58:30 +0300

    правильный способ передачи

--- classes.py
+++ classes.py
@@ -1,9 +1,9 @@
 class Person:
     
-    name = 'John'
+    # name = 'John'
 
-    def __init__(self) -> None:
-        print('hi')
+    def __init__(self, name) -> None:   #
+        self.name = name                # способ передачи с помощью параметра, теперь имена пишутся в ()
 
     def print_info(self):
         print(f'Hello, my name is {self.name}')
--- l_37_Class_Konstruktor.py
+++ l_37_Class_Konstruktor.py
@@ -3,9 +3,9 @@
 from classes import Person  # два варианта импорта класса из файла classses.py 
 # import classes              # эта запись короче и импортирует все классы 
 
-person1 = Person()
+person1 = Person('John')
 person1.print_info()
 
-person2 = Person()
-person2.name = 'Katty'
+person2 = Person('Katty')
+# person2.name = 'Katty'
 person2.print_info()
