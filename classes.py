80450ab9f722a925e285aeb557b47e191d543454
Tipn <tito.unr@gmail.com>
2021-10-12 14:06:21 +0300

    правильный способ передачи!

--- classes.py
+++ classes.py
@@ -2,8 +2,8 @@ class Person:
     
     # name = 'John'
 
-    def __init__(self, name) -> None:   #
+    def __init__(self, name) -> None:   # метод для инициализации атрибутов класса для передачи параметров
         self.name = name                # способ передачи с помощью параметра, теперь имена пишутся в ()
 
-    def print_info(self):
+    def print_info(self):               # self указывает на объект и берет свойства у конкретного объекта 
         print(f'Hello, my name is {self.name}')
