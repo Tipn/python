09a4ee754669ffd0192455c59a7a5af17304c9b7
Tipn <tito.unr@gmail.com>
2021-10-12 13:43:03 +0300

    вывел инфу из класса

--- l_37_Class_Konstruktor.py
+++ l_37_Class_Konstruktor.py
@@ -1,4 +1,7 @@
 # конструктор класса - что такое конструктор? 
 
 from classes import Person  # два варианта импорта класса из файла classses.py 
-import classes              # эта запись короче и импортирует все классы 
+# import classes              # эта запись короче и импортирует все классы 
+
+person1 = Person()
+person1.print_info()
