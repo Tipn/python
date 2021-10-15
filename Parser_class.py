9c64b4745011e59bdf2b7567405ba45e74ce5be6
Tipn <tito.unr@gmail.com>
2021-10-15 10:15:28 +0300

    adition

--- Parser_class.py
+++ Parser_class.py
@@ -10,3 +10,12 @@ class Parser:
     def __init__(self, url, path):
         self.url = url
         self.path = path 
+    
+    def get_html(self):
+        req = urllib.request.urlopen(self.url)
+        self.raw_html = req.read()
+
+    def run (self):
+        self.get_html()
+
+
--- l_39_Class_Parsing.py
+++ l_39_Class_Parsing.py
@@ -1,3 +1,6 @@
 # класс парсинга
 # создаем класс для парсинга из 34 урока  
 from Parser_class import Parser
+
+parser = Parser('https://www.ua-football.com/sport', 'news.txt')
+parser.run()
