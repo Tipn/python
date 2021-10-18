f79054118196b9258670488940d5f7fefda56fde
Tipn <tito.unr@gmail.com>
2021-10-18 17:02:27 +0300

    1

--- l_39_Class_Parsing.py
+++ l_39_Class_Parsing.py
@@ -4,5 +4,6 @@ from Parser_class import Parser
 
 parser = Parser('https://www.ua-football.com/sport', 'news.txt')
 parser.run()
-print(parser.raw_html)
-print(parser.html)
+# print(parser.raw_html)
+# print(parser.html)
+print(parser.resalts)
