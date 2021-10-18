1e82c20bd5798691d29b6db89c19299961eb7ac5
Tipn <tito.unr@gmail.com>
2021-10-18 17:01:23 +0300

    funktion to save the data

--- Parser_class.py
+++ Parser_class.py
@@ -17,7 +17,17 @@ class Parser:
         self.html = BeautifulSoup(self.raw_html, 'html.parser')
 
     def parsing(self):
-        news = soup.find_all('li', class_='liga-news-items')
+        news = self.html.find_all('li', class_='liga-news-items') # мы ищим все элементы li с классом =
+
+        for item in news: # ф. которая записывает данные в файл 
+            title = item.find('span', class_='d-block').get_text(strip=True)
+            desc = item.find('span', class_='name-dop').get_text(strip=True)
+            href = item.a.get('href')
+            self.results.append({
+                'title': title,
+                'desc': desc,
+                'href': href,
+    
 
     def run (self):
         self.get_html()
